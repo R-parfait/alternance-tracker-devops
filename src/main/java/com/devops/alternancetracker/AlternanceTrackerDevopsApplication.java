1d9675448ca3f4e10e4f8150a17485326ad601b8
Roger-Parfait Mbaitorndia <rmbaitorndia@gitlab.insa-rouen.fr>
2026-06-11 01:04:15 +0200

    structure du projet avec spring boot

---
+++ src/main/java/com/devops/alternancetracker/AlternanceTrackerDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.alternance_tracker_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternanceTrackerDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlternanceTrackerDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/alternance_tracker_devops/AlternanceTrackerDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.alternance_tracker_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlternanceTrackerDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
